43d1bce615925bc3abda4fec7100fce59814e337
hieund <hieundpython@gmail.com>
2024-09-04 23:30:12 +0700

    add json

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -10,5 +10,11 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "show movie handler")
+	id, err := app.readIDParam(r)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "show movie handler %d", id)
 }
